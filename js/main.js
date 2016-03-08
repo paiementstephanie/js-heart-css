eca8084d7a5a85664a315c254c8b3d228ad793c3
paiementstephanie <stephanie.paiement@rogers.com>
2016-03-08 15:51:20 -0500

    adding elements

--- js/main.js
+++ js/main.js
@@ -1,2 +1,12 @@
 var $header = $('header');
 var $h1 = $('h1');
+var $main = $('main');
+
+$('header').add('masthead');
+
+$('h1').remove('big-heading').add('logo');
+
+$('main').add('wrapper');
+
+
+

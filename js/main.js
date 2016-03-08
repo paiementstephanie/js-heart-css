c4dffdcd303a0de101fce81ec25927852023e0fb
paiementstephanie <stephanie.paiement@rogers.com>
2016-03-08 16:37:40 -0500

    changes to$shortcuts

--- js/main.js
+++ js/main.js
@@ -1,12 +1,29 @@
 var $header = $('header');
 var $h1 = $('h1');
+var $h2 = $('h2');
 var $main = $('main');
+var $images = $('img');
 
-$('header').add('masthead');
+$header.addClass('masthead');
 
-$('h1').remove('big-heading').add('logo');
+$h1.removeClass('big-heading').addClass('logo');
 
-$('main').add('wrapper');
+$main.addClass('wrapper');
 
+$('main p:first-child').addClass('intro');
 
+$h2.addClass('slug-head');
 
+$('main ul').addClass('slug-list');
+
+$images.addClass('slug-img');
+
+$('main li:first-child').addClass('slug-list-first');
+
+$('main li:last-child').addClass('slug-list-last');
+
+$('dl').addClass('classification');
+
+$('dt').removeClass('big-label');
+
+$('ul li:last-child').show();
